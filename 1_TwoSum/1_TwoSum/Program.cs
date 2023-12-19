ade7319f4eb38fd4f079a39ee6e01908a838549b
Vitor França <francavit.r@gmail.com>
2023-12-19 17:06:15 -0300

    Problemas 1, 2, 4, 7, 9

---
+++ 1_TwoSum/1_TwoSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_TwoSum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 7, 11, 5 };
+            TwoSum(arr, 9);
+
+
+            int[] TwoSum(int[] nums, int target)
+            {
+                int firstNum = 0;
+                while (firstNum <= nums.Length - 1)
+                {
+                    for (int i = firstNum + 1; i < nums.Length; i++)
+                    {
+                        if (nums[firstNum] + nums[i] == target)
+                        {
+
+                            return new int[2] { firstNum, i };
+                        }
+
+
+                    }
+                    firstNum++;
+                }
+                return new int[0];
+            }
+        }
+    }
+}
---
+++ 7_ReverseInteger/7_ReverseInteger/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ReverseInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int teste1 = 123;
+            int teste2 = -123;
+            int teste3 = 120;
+
+
+            Console.WriteLine(Reverse(teste1));
+            Console.WriteLine(Reverse(teste2));
+            Console.WriteLine(Reverse(teste3));
+
+            Console.ReadKey();
+        }
+
+        public static int Reverse(int x)
+        {
+            if (x == 0)
+            {
+                return 0;
+            }
+
+            StringBuilder intNormal = new StringBuilder(x.ToString());
+            StringBuilder intReversed = new StringBuilder();
+            char aux = intNormal[intNormal.Length - 1];
+
+            
+
+            if (intNormal[0] == '-')
+            {
+                intReversed = intReversed.Append('-');
+                intNormal = intNormal.Remove(0, 1);
+            }
+
+            while (aux == '0')
+            {
+                intNormal = intNormal.Remove(intNormal.Length - 1, 1);
+                aux = intNormal[intNormal.Length - 1];
+            }
+
+            while (intNormal.Length > 0)
+            {
+                intReversed.Append(aux);
+                intNormal = intNormal.Remove(intNormal.Length - 1, 1);
+
+                if (intNormal.Length != 0)
+                {
+                    aux = intNormal[intNormal.Length - 1];
+                }
+            }
+
+            long reversed = Convert.ToInt64(intReversed.ToString());
+
+            if (reversed > 2147483647 || reversed < -2147483648 || reversed == 0)
+            {
+                return 0;
+            }
+
+            return Convert.ToInt32(intReversed.ToString());
+
+            
+        }
+
+
+    }
+}
