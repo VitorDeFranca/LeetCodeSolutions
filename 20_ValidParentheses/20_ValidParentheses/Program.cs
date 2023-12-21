99f613935dec2d9474fb13daba9942065395f75d
Vitor França <francavit.r@gmail.com>
2023-12-21 14:00:32 -0300

    Problema 20 e 1108 adicionados

---
+++ 20_ValidParentheses/20_ValidParentheses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_ValidParentheses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IsValid("()");
+            IsValid("() [] {}");
+            IsValid("(]");
+
+        }
+
+        public static bool IsValid(string s)
+        {
+            if (s.Length % 2 == 1) return false;
+
+            char[] cs = s.Replace(" ", "").ToCharArray();
+            Stack<char> stack = new Stack<char>();
+
+
+            for (int i = 0; i < cs.Length; i++)
+            {
+                if (cs[i] == '(' || cs[i] == '[' || cs[i] == '{')
+                {
+                    stack.Push(cs[i]);
+                }
+                else
+                {
+                    if (stack.Count == 0) return false;
+
+
+                    if (stack.Peek() == '(')
+                    {
+                        if ((int)cs[i] != ((int)stack.Peek() + 1)) return false;
+                    }
+
+                    if (stack.Peek() == '[' || stack.Peek() == '{')
+                    {
+                        if ((int)cs[i] != ((int)stack.Peek() + 2)) return false;
+                    }
+
+                    stack.Pop();
+                }
+            }
+
+            return stack.Count == 0;
+
+        }
+    }
+}
